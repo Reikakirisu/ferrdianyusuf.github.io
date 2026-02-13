a291c5b2c4feac5ab64b8d7d086cac1af1a5fdde
Reikakirisu <121605745+Reikakirisu@users.noreply.github.com>
2026-02-13 23:01:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://anjay3323.edgeone.app/Goo%20Goo%20Dolls%20%20Iris%20[Official%20Music%20Video]%20[4K%20Remaster].mp3", // Music streaming URL
+        musicUrl: "https://jumpshare.com/share/gqYlKwX3moXdyUTVZVf0?b=TOCj91hKT4Q2DU8vOnNn", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
